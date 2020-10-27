2009d557160ea615f4b8db1f5334329eb9e89412
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2020-10-27 23:04:11 +0300

    Fixed C link

--- discord_rpc/discord_rpc.py
+++ discord_rpc/discord_rpc.py
@@ -13,33 +13,34 @@ client_id = '744403269237080127' #'541005130749968405'  # App ID. Change if you 
 
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
-
+file = ""
 # Class of extension
 class DiscordRpc(Extension):
     def __init__(self, parent):
+        super().__init__(parent)
         self.file = ""
         self.timer = QTimer(self)
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.update_RPC)
-        super().__init__(parent)
 
-    def update_RPC():
+    def update_RPC(self):
+        global file
         try:
             # Detecting new document
             if(Krita.instance().activeDocument() != None):
-                if(self.file != Krita.instance().activeDocument().fileName()):
+                if(file != Krita.instance().activeDocument().fileName()):
                     RPC.update(details="Draws something cool", state=str(Krita.instance().activeDocument().name()) or "No name", large_image="krita_logo", start=int(time.time()))
-                    self.file = Krita.instance().activeDocument().fileName()
+                    file = Krita.instance().activeDocument().fileName()
             else:
                 RPC.update(details="Idle", large_image="krita_logo")
-                self.file = ""
+                file = ""
         except Exception as e:
             # Хз как вывести эксепшн нормально
             with open("C:\\Users\\panko\\Desktop\\log.txt", "a") as f:
                f.write("%s\n" % time.time())
                f.write(traceback.format_exc())
             # RPC.update(details="An exception occured", state=type(e).__name__ + ": " + str(e), large_image="krita_logo")
-            # self.file = ""
+            # file = ""
             pass
 
     def setup(self):
@@ -58,4 +59,3 @@ class DiscordRpc(Extension):
 app = Krita.instance()
 extension = DiscordRpc(parent=app) 
 app.addExtension(extension)
-
