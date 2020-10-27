500378ed8eb2873bf4ea109fe1d9fe8dc4de8f0e
HeroBrine1st Erquilenne <herobrine1st@pm.me>
2020-10-27 18:02:46 +0300

    Fixed: if name of document was zero string, plugin throws an exception and shuts down until restart
    If exception occured, shows in RPC for 5 seconds
    There's still many unknown exceptions (at least 1, but can't reproduce error)

--- discord_rpc/discord_rpc.py
+++ discord_rpc/discord_rpc.py
@@ -16,37 +16,39 @@ RPC.connect() # Start the handshake loop
 # The status thread 
 class DiscordStatusThread (threading.Thread):
    def __init__(self):
-      threading.Thread.__init__(self)
-      self.updatedStatus = False
-      self.loopping = True
+      threading.Thread.__init__(self, name="DRP Plugin Thread")
       self.singleOpen = 0
+      self.file = ""
    def run(self):
-      while self.loopping:
+      while True:
         try:
             # Closing this thread
             if(Krita.instance().activeWindow() == None):
                 if(self.singleOpen >= 1):
-                    self.loopping = False
+                    break
 
                 if(self.singleOpen == 0):
                     self.singleOpen = 1
 
             # Detecting new document
             if(Krita.instance().activeDocument() != None):
-                if(self.updatedStatus == False):
-                    RPC.update(details="Working on", state=str(Krita.instance().activeDocument().name()), large_image="krita_logo", start=int(time.time()))
-                    self.updatedStatus = True
+                if(self.file != Krita.instance().activeDocument().fileName()):
+                    RPC.update(details="Draws something cool", state=str(Krita.instance().activeDocument().name()) or "No name", large_image="krita_logo", start=int(time.time()))
+                    self.file = Krita.instance().activeDocument().fileName()
             else:
-                RPC.update(details="Not currently drawing", large_image="krita_logo")
-                self.updatedStatus = False
+                RPC.update(details="Idle", large_image="krita_logo")
+                self.file = ""
             time.sleep(5)
-        except:
-            self.loopping = False
+        except Exception as e:
+            # Хз как вывести эксепшн нормально
+            RPC.update(details="An exception occured", state=type(e).__name__ + ": " + str(e), large_image="krita_logo")
+            self.file = ""
+            time.sleep(5)
 
 # Class of extension
 class DiscordRpc(Extension):
     def __init__(self, parent):
-        RPC.update(details="Not currently drawing", large_image="krita_logo")
+        RPC.update(details="Idle", large_image="krita_logo")
         super().__init__(parent)
 
     def setup(self):
